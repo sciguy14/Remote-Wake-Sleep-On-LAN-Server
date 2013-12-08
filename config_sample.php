d81da9be44c81015a90bd6cd0ad143633e6fd19a
ahodgey <andrew.dev@ahah.org.uk>
2013-12-08 08:40:33 +0000

    Wake multiple PCs & SleepOnLan change
    Add functionality to wake multiple PCs with simple dropdown
    Add option to change the sleep method issued to the SleepOnLan software
    Changed submit button (wake, sleep) name so didnt interfere with
    javascript on dropdown

--- config_sample.php
+++ config_sample.php
@@ -15,16 +15,26 @@
 	
 	//This is the number of times that the WOL server will try to ping the target computer to check if it has woken up. Default = 10.
 	$MAX_PINGS = 10;
+	//This is the number of seconds to wait between pings commands when waking up or sleeping , depending on whether starting from sleep or shutdwon may take longer
+	$SLEEP_TIME = 5;
+
+	//This is the Name of the computers to appear in the drop down
+	$COMPUTER_NAME = array("computer1","computer2");
 	
 	//This is the MAC address of the Network Interface on the computer you are trying to wake.
-	$COMPUTER_MAC = '00:00:00:00:00:00';
+	$COMPUTER_MAC = array("00:00:00:00:00:00:00","00:00:00:00:00:00:00");
 	
 	//This is the LOCAL IP address of the computer you are trying to wake.  Use a reserved DHCP through your router's administration interace to ensure it doesn't change.
-	$COMPUTER_LOCAL_IP = '192.168.1.100';
+	$COMPUTER_LOCAL_IP = array("192.168.0.1","192.168.0.2");
+
 	
 	//This is the Port being used by the Windows SleepOnLan Utility to initiate a Sleep State
 	//http://www.ireksoftware.com/SleepOnLan/
 	$COMPUTER_SLEEP_CMD_PORT = 7760;
+	//Command to be issued by the windows sleeponlan utility 
+	//options are suspend, hibernate, logoff, poweroff, forcepoweroff, lock, reboot
+	//if using power off can create a windows scheduled task that call sleeponlan.exe with following startup parameters /auto /port=7760
+	$COMPUTER_SLEEP_CMD = "poweroff";
 	
 	//This is the location of the bootstrap style folder relative to your index and config file. Default = "" (Same folder as this file)
 	//Directory must be called "bootstrap". You may wish to move if this WOL script is the "child" of a larger web project on your Pi, that will also use bootstrap styling.
